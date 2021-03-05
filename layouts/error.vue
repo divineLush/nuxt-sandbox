a7bce981da7fbd112411b27d0f5a35bf871c79cf
divineLush <homeoflucifer@gmail.com>
2021-03-05 21:11:17 +0700

    adding custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,8 @@
+<template>
+    <!-- error is a reserved name -->
+    <!-- loads every time nuxt throws an error -->
+    <section>
+        <h1>An error occured, you're screwed</h1>
+        <nuxt-link to="/">Nobody loves you(go home)</nuxt-link>
+    </section>
+</template>
