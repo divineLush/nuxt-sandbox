0af0fb207a308bdbc7ee700a172870dcad839c96
divineLush <homeoflucifer@gmail.com>
2021-03-05 21:25:06 +0700

    minor improvements

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,4 +21,13 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
+
+h1 {
+  margin: 10px 0;
+  color: green;
+}
+
+h2 {
+  margin: 6px 0;
+}
 </style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -3,6 +3,6 @@
     <!-- loads every time nuxt throws an error -->
     <section>
         <h1>An error occured, you're screwed</h1>
-        <nuxt-link to="/">Nobody loves you(go home)</nuxt-link>
+        <nuxt-link to="/">Get some rest</nuxt-link>
     </section>
 </template>
