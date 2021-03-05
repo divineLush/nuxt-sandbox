acd65abd6149627c0f98330e9e026eefa6d78d91
divineLush <homeoflucifer@gmail.com>
2021-03-05 20:29:21 +0700

    init

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <Nuxt />
+  </main>
+</template>
+
+<style>
+html {
+  font-family: monospace;
+  font-size: 1.3rem;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
