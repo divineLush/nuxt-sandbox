b5a345a0a541afbdce046f3e841ec6fa77f1f77b
divineLush <homeoflucifer@gmail.com>
2021-03-05 20:52:01 +0700

    nested routes

---
+++ pages/products.vue
@@ -0,0 +1,10 @@
+<template>
+    <!-- This template is a wrapper for both products/index.vue and any other nested route -->
+    <section>
+        <h1>Products Page</h1>
+        <nuxt-link to="/products/123">Go to products/123</nuxt-link>
+        <nuxt-link to="/products/qwerty">Invalid link to products/qwerty</nuxt-link>
+        <!-- marks the place where the nested routes should be loaded -->
+        <nuxt-child></nuxt-child>
+    </section>
+</template>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,7 +1,5 @@
 <template>
     <section>
         <h1>Products Home</h1>
-        <nuxt-link to="products/123">Go to products/123</nuxt-link>
-        <nuxt-link to="products/qwerty">Invalid link to products/qwerty</nuxt-link>
     </section>
 </template>
